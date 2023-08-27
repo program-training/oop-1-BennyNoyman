3944f29f09563fb6085966e330afa26382cae65d
BennyNoyman <bn0504127633@gmail.com>
2023-08-27 10:45:05 +0300

    Rectangle & Sqare

--- index.ts
+++ index.ts
@@ -1,9 +1,23 @@
 // את הקוד שלכם תכתבו כאן
 
 //  1. Rectangle - מלבן
-
+class Rectangle {
+    width: number;
+    height: number
+    constructor(width: number, height: number) {
+        this.width = width;
+        this.height = height;
+    }
+    getArea(width: number, height: number): number{
+        return this.width * this.height;
+    }
+}
 //  2. Square - ריבוע
-
+class Square extends Rectangle{
+    constructor(width: number) {
+        super(width, width);
+    }
+}
 // 3. Shape - צורה
 
 // 4. Method Chaining
